651251da19d42b39ac044656cca191fb48ae8eb0
Srij1 <90286700+Srij1@users.noreply.github.com>
2023-02-06 15:38:56 -0600

    Update code.py

--- code.py
+++ code.py
@@ -7,31 +7,33 @@ def divide(d1, d2):
 def add(a1, a2):
     print("The answer is: ", a1+a2)
 
-def sub(s1, s2):
+def subtract(s1, s2):
     print("The answer is: ", s1-s2)
 
-choice = input("Choose what kind of action you wish to perform. M to Multiply, D to divide, S to subtract, or S to add:")
+while True:
+  choice = input("Choose what kind of action you wish to perform. M to Multiply, D to divide, or s to subtract:")
 
-if choice == "M":
-    print("You have chosen to multiply.")
-    m1 = int(input("Enter the first number to multiply: "))
-    m2 = int(input("Enter the second number to multiply: "))
-    multiply(m1,m2)
+  if choice == "M":
+      print("You have chosen to multiply.")
+      m1 = int(input("Enter the first number to multiply: "))
+      m2 = int(input("Enter the second number to multiply: "))
+      multiply(m1,m2)
 
-if choice == "D":
-    print("You have chosen to divide.")
-    d1 = int(input("Enter the number you want to be divded: "))
-    d2 = int(input("Enter the number that divdes the first: "))
-    divide(d1,d2)
+  if choice == "D":
+      print("You have chosen to divide.")
+      d1 = int(input("Enter the number you want to be divded: "))
+      d2 = int(input("Enter the number that divdes the first: "))
+      divide(d1,d2)
 
-if choice == "A":
-    print("You have chosen to add.")
-    a1 = int(input("Enter the first addend: "))
-    a2 = int(input("Enter the second addend: "))
-    add(a1,a2)
+  if choice == "A":
+      print("You have chosen to add.")
+      a1 = int(input("Enter the first addend: "))
+      a2 = int(input("Enter the second addend: "))
+      add(a1,a2)
 
-if choice == "s":
-    print("You have chosen to subtract.")
-    s1 = int(input("Enter the number you want to subtract: "))
-    s2 = int(input("Enter the number that subtracts the first: "))
-    subtract(s1,s2)
+  if choice == "s":
+      print("You have chosen to subtract.")
+      s1 = int(input("Enter the number you want to subtract: "))
+      s2 = int(input("Enter the number that subtracts the first: "))
+      subtract(s1,s2)
+      
